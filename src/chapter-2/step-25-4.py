f3934fa05d40e5b85e9fc0edf959d5582acb1594
Igor Dubinin <dubininIv@sber-service.ru>
2022-06-23 22:55:07 +0300

    Remember predict

--- src/chapter-2/step-25-4.py
+++ src/chapter-2/step-25-4.py
@@ -4,22 +4,62 @@
 
 import pandas as pd
 
-P = [1,0,5,2,2]
+class Vector252:
+    def __init__(self):
+        self.P1 = (0,1,2)
+        self.P2 = (2,1,0)
 
-def normal(value, mean, std):
-    return (value - mean) / std
+    def euclideanDistance(self):
+        sumOfSquare = 0
+        for i in range(3):
+            sumOfSquare += (self.P1[i] - self.P2[i])**2
+        return round(sumOfSquare**0.5, 2)
+
+    def manhattan(self):
+        sumOfAbs = 0
+        for i in range(3):
+            sumOfAbs += abs(self.P1[i] - self.P2[i])
+        return sumOfAbs
+
+    def maxMetric(self):
+        diffs = [abs(self.P1[i] - self.P2[i]) for i in range(3)]
+        return max(diffs)
+
+class Vector253:
+    def __init__(self):
+        self.P = (1, 0, 5, 2, 2)
+        self.df = pd.DataFrame(self.P)
+        self.max = self.df.max()[0]
+        self.min = self.df.min()[0]
+        self.mean = self.df.mean().round(2)[0]
+        self.std = self.df.std().round(2)[0]
+
+    def normalizeMaxMin(self, value):
+        return (value - self.min) / (self.max - self.min)
+
+    def getNormalizedMaxMin(self):
+        return list(self.normalizeMaxMin(self.P[i]) for i in range(len(self.P)))
+
+    def normalizeMean(self, value):
+        return round((value - self.mean) / self.std, 2)
+
+    def getNormalizedMean(self):
+        return list(self.normalizeMean(self.P[i]) for i in range(len(self.P)))
+
 
 def main():
-    df = pd.DataFrame(data={'p' : P})
-    std = df.std()
-    mean = df.mean()
-    metric = normal(P[-1], mean, std)
+    vector252 = Vector252()
+    print(
+        'euclideanDistance', vector252.euclideanDistance(),
+        'manhattan', vector252.manhattan(),
+        'euclideanDistance', vector252.maxMetric()
+    )
 
-    print('std', std, 'mean', mean, 'metric', metric)
-
-    for i in range(len(P)):
-        print('value', P[i], normal(P[i], mean, std))
-
+    vector253 = Vector253()
+    print(
+        'getNormalizedMaxMin', vector253.getNormalizedMaxMin(),
+        'getNormalizedAverage', vector253.getNormalizedMean()
+    )
 
 if __name__ == '__main__':
     main()
--- src/chapter-2/step-25-5.py
+++ src/chapter-2/step-25-5.py
@@ -4,45 +4,74 @@
 Требуется оценить, какую оценку поставит Саша фильму «Гарри Поттер». Сделаем это с помощью метрики Манхэттен (для простоты вычислений данные в таблице нормировать не нужно). Для этого подсчитаем расстояния от Саши до других людей, используя информацию из первых 3 столбцов. Чему равна ожидаемая оценка для «Гарри Поттера» (округлить до одного знака после запятой)?
 '''
 
-movies = [
-    {
-        'name': 'The Silence of the Lambs',
-        'scores': { 'Vasia': 5, 'Petia': 5, 'Masha': 2, 'Sasha': 3, }
-    },
-    {
-        'name': 'Titanic',
-        'scores': { 'Vasia': 5, 'Petia': 3, 'Masha': 5, 'Sasha': 4, }
-    },
-    {
-        'name': 'Matrix',
-        'scores': { 'Vasia': 5, 'Petia': 4, 'Masha': 3, 'Sasha': 4, }
-    },
-    {
-        'name': 'Harry Potter',
-        'scores': { 'Vasia': 3, 'Petia': 4, 'Masha': 5, 'Sasha': -1, }
-    },
+users = [
+    'Vasya',
+    'Petya',
+    'Masha',
+    'Sasha',
 ]
 
+movies = [
+    'The Silence of the Lambs',
+    'Titanic',
+    'Matrix',
+    'Harry Potter',
+]
+
+rating = {
+    'Vasya': {
+        'The Silence of the Lambs': 5,
+        'Titanic': 5,
+        'Matrix': 5,
+        'Harry Potter': 3,
+    },
+    'Petya': {
+        'The Silence of the Lambs': 5,
+        'Titanic': 3,
+        'Matrix': 4,
+        'Harry Potter': 4,
+    },
+    'Masha': {
+        'The Silence of the Lambs': 2,
+        'Titanic': 5,
+        'Matrix': 3,
+        'Harry Potter': 5,
+    },
+    'Sasha': {
+        'The Silence of the Lambs': 3,
+        'Titanic': 4,
+        'Matrix': 4,
+        'Harry Potter': None,
+    },
+}
+
+def predict():
+    uList = ('Vasya','Petya', 'Masha')
+    metrics = {
+        'Vasya': 0,
+        'Petya': 0,
+        'Masha': 0,
+    }
+    for user in uList:
+        manhattan = 0
+        for movie in ('The Silence of the Lambs', 'Titanic', 'Matrix'):
+            manhattan += abs(rating['Sasha'][movie] - rating[user][movie])
+        metrics[user] = manhattan
+
+    p1 = 0
+    for user in uList:
+        p1 += 1 / metrics[user]
+    p1 = 1 / p1
+
+    p2 = 0
+    for user in uList:
+        p2 += rating[user]['Harry Potter'] / metrics[user]
+
+    return round(p1 * p2, 1)
+
 def main():
-    metrics = []
-
-    for name in ['Vasia', 'Petia', 'Masha']:
-        metric = 0
-        for i in range(3):
-            movie = movies[i]
-            metric += abs(movie['scores'][name] - movie['scores']['Sasha'])
-        metrics.append(metric)
-
-    print('metrics', metrics)
-
-    left = 1 / (1 / metrics[0] + 1/metrics[1] + 1/metrics[2])
-    right = (
-        movies[3]['scores']['Vasia'] / metrics[0]
-        + movies[3]['scores']['Petia'] / metrics[1]
-        + movies[3]['scores']['Masha'] / metrics[2]
-    )
-
-    print(left * right)
+    p = predict()
+    print('p', p)
 
 if __name__ == '__main__':
     main()
